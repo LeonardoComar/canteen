d456486edb8d4dd9db9d49393c985491dd3c42fd
LeonardoComar <leofcomar@gmail.com>
2022-07-15 14:47:47 -0300

    Controller e view de Customer

---
+++ app/controllers/main/customers_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class Main::CustomersController < ApplicationController
+  before_action :set_customer, only: %i[show]
+
+  def index
+    @customers = Customer.where(status: 'active')
+  end
+
+  def show
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save!
+      flash[:success] = I18n.t(:created_successfully, scope: %i[_dictionary], resource_name: I18n.t(:customer, scope: %i[activerecord models], count: 1))
+      redirect_to main_customer_path(@customer)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :matriculation_code, :password, :password_confirmation)
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+end

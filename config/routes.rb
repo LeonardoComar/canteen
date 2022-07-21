8bccbacf5f115d9b77f8c8303f855363868aa283
LeonardoComar <leofcomar@gmail.com>
2022-07-21 10:59:46 -0300

    Resolvendo erros de validações

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@
 
 Rails.application.routes.draw do
   root "home#index"
+  resources :customers, path: 'clientes'
   
   devise_for :users
-
+  
   namespace :main, path: '', path_names: { new: 'novo', create: 'novo', edit: 'editar', update: 'editar' } do
-    resources :customers, path: 'clientes'
   end
 end
